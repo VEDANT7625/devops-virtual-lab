be46924d2162d5602bb0edc8d9e69e06c6c6d3cf
Vedant Kumar <vedkr981@gmail.com>
2026-03-16 07:26:05 +0530

    Initial DevOps CI/CD Pipeline

---
+++ terraform/main.tf
@@ -0,0 +1,13 @@
+provider "aws" {
+ region = "ap-south-1"
+}
+
+resource "aws_instance" "devops_server" {
+ ami = "ami-0e670eb768a5fc3d4"
+ instance_type = "t3.micro"
+ key_name = "devops-key"
+
+ tags = {
+   Name = "DevOps-Lab-Server"
+ }
+}
